86dfc28d82adb829a1a9350f641a46991385c095
Sho Hashimoto <hashimoto@shokai.org>
2011-11-06 23:16:08 +0900

    http server

--- nfc-tag-server.rb
+++ nfc-tag-server.rb
@@ -3,22 +3,40 @@ require 'rubygems'
 require 'nfc'
 require 'eventmachine'
 require 'em-websocket'
+require 'evma_httpserver'
+
+@@tag = nil
+
+class NfcHttpServer  < EventMachine::Connection
+  include EventMachine::HttpServer
+  
+  def process_http_request
+    res = EventMachine::DelegatedHttpResponse.new(self)
+    puts "* http #{@http_request_method} #{@http_path_info} #{@http_query_string} #{@http_post_content}"
+    res.status = 200
+    res.content = @@tag
+    res.send_response
+  end
+end
+
 
 EM::run do
   channel = EM::Channel.new
+
+  EM::start_server('0.0.0.0', 8081, NfcHttpServer)
   EM::WebSocket.start(:host => '0.0.0.0', :port => 8080) do |ws|
     ws.onopen do
       sid = channel.subscribe do |mes|
         ws.send mes
       end
       puts "* new client <#{sid}> connected"
-
+      ws.send @@tag
       ws.onmessage do |mes|
         puts "* client <#{sid}> says : #{mes}"
       end
 
       ws.onclose do
-        @channel.unsubscribe sid
+        channel.unsubscribe sid
         puts "* client <#{sid}> closed"
       end
     end
@@ -27,13 +45,12 @@ EM::run do
   EM::defer do
     nfc = NFC.instance
     puts "reader : #{nfc.device.name}"
-    tag_p = nil
     loop do
-      tag = nfc.find.to_s
-      if tag != tag_p
+      tag = nfc.find.to_s.downcase
+      if tag != @@tag
         puts "tag : #{tag}"
         channel.push tag.to_s
-        tag_p = tag
+        @@tag = tag
       end
       sleep 0.1
     end
